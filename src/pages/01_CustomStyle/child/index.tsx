6de5d2e1a6e09f31ae6bae7c9c06310d04f9d254
yuqing.yang_UI-DH <yuqing.yang@ui-dh.com>
2023-07-18 14:02:05 +0800

    上传dist

--- src/pages/01_CustomStyle/child/index.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import styles from './index.less';
-const Child = (props) => {
-  const { extraClassName } = props;
-  return <div className={`${styles.myclass} ${extraClassName}`}>child</div>;
-};
-
-export default Child;
--- src/pages/02_GlobalCSS/index.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const GlobalCSS = () => {
-  return <div className="mycss">GlobalCSS</div>;
-};
-
-export default GlobalCSS;
